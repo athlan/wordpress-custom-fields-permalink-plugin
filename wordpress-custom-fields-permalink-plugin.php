1de17712ec16f39564006c850386d339f6c8f8d7
Athlan <athlan@vgroup.pl>
2015-07-16 18:50:41 +0200

    Formatting code.

--- wordpress-custom-fields-permalink-plugin.php
+++ wordpress-custom-fields-permalink-plugin.php
@@ -10,88 +10,88 @@ Author URI: http://athlan.pl/
 
 class CustomFieldsPermalink {
 
-	const PARAM_CUSTOMFIELD_KEY = 'custom_field_key';
-	const PARAM_CUSTOMFIELD_VALUE = 'custom_field_value';
+    const PARAM_CUSTOMFIELD_KEY = 'custom_field_key';
+    const PARAM_CUSTOMFIELD_VALUE = 'custom_field_value';
     
     public static $checkCustomFieldValue = false;
-	
-	public static function linkPost($permalink, $post, $leavename) {
-		return self::linkRewriteFields($permalink, $post);
-	}
-	
-	public static function linkPostType($permalink, $post, $leavename, $sample) {
-		return self::linkRewriteFields($permalink, $post);
-	}
-	
-	protected static function linkRewriteFields($permalink, $post) {
-		return preg_replace('#(%field_(.*?)%)#e', 'CustomFieldsPermalink::linkRewriteFieldsExtract($post, "\\2")', $permalink);
-	}
-	
-	public static function linkRewriteFieldsExtract($post, $fieldName) {
-		$postMeta = get_post_meta($post->ID);
-		
-		if(!isset($postMeta[$fieldName]))
-			return '';
-		
-		$value = implode('', $postMeta[$fieldName]);
+    
+    public static function linkPost($permalink, $post, $leavename) {
+        return self::linkRewriteFields($permalink, $post);
+    }
+    
+    public static function linkPostType($permalink, $post, $leavename, $sample) {
+        return self::linkRewriteFields($permalink, $post);
+    }
+    
+    protected static function linkRewriteFields($permalink, $post) {
+        return preg_replace('#(%field_(.*?)%)#e', 'CustomFieldsPermalink::linkRewriteFieldsExtract($post, "\\2")', $permalink);
+    }
+    
+    public static function linkRewriteFieldsExtract($post, $fieldName) {
+        $postMeta = get_post_meta($post->ID);
+        
+        if(!isset($postMeta[$fieldName]))
+            return '';
+        
+        $value = implode('', $postMeta[$fieldName]);
         
         $value = sanitize_title($value);
         
         return $value;
-	}
-	
-	public static function registerExtraQueryVars($value) {
-		array_push($value, self::PARAM_CUSTOMFIELD_KEY, self::PARAM_CUSTOMFIELD_VALUE);
-		return $value;
-	}
-	
-	public static function processRequest($value) {
-		// additional parameters added to Wordpress
-		// Main Loop query
-		$value['meta_key'] = $value[self::PARAM_CUSTOMFIELD_KEY];
+    }
+    
+    public static function registerExtraQueryVars($value) {
+        array_push($value, self::PARAM_CUSTOMFIELD_KEY, self::PARAM_CUSTOMFIELD_VALUE);
+        return $value;
+    }
+    
+    public static function processRequest($value) {
+        // additional parameters added to Wordpress
+        // Main Loop query
+        $value['meta_key'] = $value[self::PARAM_CUSTOMFIELD_KEY];
         
         // do not check field's value for this moment
         if(true === self::$checkCustomFieldValue) {
             $value['meta_value'] = $value[self::PARAM_CUSTOMFIELD_VALUE];
-		}
+        }
         
-		// remove temporary injected parameters
-		unset($value[self::PARAM_CUSTOMFIELD_KEY], $value[self::PARAM_CUSTOMFIELD_VALUE]);
-		
-		return $value;
-	}
-	
-	public static function rewriteRulesArrayFilter($rules) {
-		$keys = array_keys($rules);
-		$tmp = $rules;
-		$rules = array();
-		
-		for($i = 0, $j = sizeof($keys); $i < $j; ++$i) {
-			$key = $keys[$i];
-			
-			if (preg_match('/%field_([^%]*?)%/', $key)) {
-				$keyNew = preg_replace(
-					'/%field_([^%]*?)%/',
-					'([^/]+)',
-					// you can simply add next group to the url, because WordPress
-					// detect them automatically and add next $matches indiceis
-					$key
-				);
-				$rules[$keyNew] = preg_replace(
-					'/%field_([^%]*?)%/',
-					sprintf('%s=$1&%s=', self::PARAM_CUSTOMFIELD_KEY, self::PARAM_CUSTOMFIELD_VALUE),
-					// here on the end will be pasted $matches[$i] from $keyNew, so we can
-					// grab it it the future in self::PARAM_CUSTOMFIELD_VALUE parameter
-					$tmp[$key]
-				);
-			}
-			else {
-				$rules[$key] = $tmp[$key];
-			}
-		}
-		
-		return $rules;
-	}
+        // remove temporary injected parameters
+        unset($value[self::PARAM_CUSTOMFIELD_KEY], $value[self::PARAM_CUSTOMFIELD_VALUE]);
+        
+        return $value;
+    }
+    
+    public static function rewriteRulesArrayFilter($rules) {
+        $keys = array_keys($rules);
+        $tmp = $rules;
+        $rules = array();
+        
+        for($i = 0, $j = sizeof($keys); $i < $j; ++$i) {
+            $key = $keys[$i];
+            
+            if (preg_match('/%field_([^%]*?)%/', $key)) {
+                $keyNew = preg_replace(
+                    '/%field_([^%]*?)%/',
+                    '([^/]+)',
+                    // you can simply add next group to the url, because WordPress
+                    // detect them automatically and add next $matches indiceis
+                    $key
+                );
+                $rules[$keyNew] = preg_replace(
+                    '/%field_([^%]*?)%/',
+                    sprintf('%s=$1&%s=', self::PARAM_CUSTOMFIELD_KEY, self::PARAM_CUSTOMFIELD_VALUE),
+                    // here on the end will be pasted $matches[$i] from $keyNew, so we can
+                    // grab it it the future in self::PARAM_CUSTOMFIELD_VALUE parameter
+                    $tmp[$key]
+                );
+            }
+            else {
+                $rules[$key] = $tmp[$key];
+            }
+        }
+        
+        return $rules;
+    }
 }
 
 add_filter('pre_post_link', array('CustomFieldsPermalink', 'linkPost'), 100, 3);
