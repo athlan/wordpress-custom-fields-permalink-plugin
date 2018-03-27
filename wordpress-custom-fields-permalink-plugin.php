9ba054222eceac9ee301dc1ecbfb225279674a77
Athlan <athlan@vgroup.pl>
2018-03-27 22:52:52 +0200

    phpcs

--- wordpress-custom-fields-permalink-plugin.php
+++ wordpress-custom-fields-permalink-plugin.php
@@ -1,19 +1,19 @@
 <?php
 /**
+ * Custom Fields Permalink 2.
+ *
+ * @package     WordPress_Custom_Fields_Permalink
+ *
+ * @wordpress-plugin
  * Plugin Name: Custom Fields Permalink 2
  * Plugin URI: http://athlan.pl/wordpress-custom-fields-permalink-plugin
  * Description: Plugin allows to use post's custom fields values in permalink structure by adding %field_fieldname%, for posts, pages and custom post types.
  * Author: Piotr Pelczar
  * Version: 1.0.3
  * Author URI: http://athlan.pl/
- *
- * @package WordPress_Custom_Fields_Permalink
  */
 
-/**
- * Main plugin binding file.
- */
-
+// Require main class.
 require 'includes/class-customfieldspermalink.php';
 
 add_filter( 'pre_post_link', array( 'CustomFieldsPermalink', 'link_post' ), 100, 3 );
