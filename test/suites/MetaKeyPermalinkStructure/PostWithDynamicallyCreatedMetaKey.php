05c59afb27b3c1fd2282eeffccd62d0d422da2f8
Athlan <athlan@vgroup.pl>
2018-04-22 20:44:56 +0200

    Handle dynamic custom fields values by wpcfp_get_post_metadata.

--- test/suites/MetaKeyPermalinkStructure/PostWithDynamicallyCreatedMetaKey.php
+++ test/suites/MetaKeyPermalinkStructure/PostWithDynamicallyCreatedMetaKey.php
@@ -11,6 +11,29 @@
 class PostWithDynamicallyCreatedMetaKey extends BaseTestCase {
 
 	/**
+	 * Sample custom metadata filter that adds some metadata to the array.
+	 *
+	 * @param array   $post_meta  The metadata returned from get_post_meta.
+	 * @param WP_Post $post       The post object.
+	 *
+	 * @return array metadata
+	 */
+	function generate_dynamic_metadata( $post_meta = null, $post = null ) {
+		if ( ! array_key_exists( 'some_meta_key', $post_meta ) ) {
+			$post_meta['some_meta_key'] = 'Default value';
+		}
+
+		return $post_meta;
+	}
+
+	/**
+	 * Test step that hook wpcfp_get_post_metadata has been registered.
+	 */
+	private function given_hook_registered() {
+		add_filter( 'wpcfp_get_post_metadata', array( $this, 'generate_dynamic_metadata' ), 1, 2 );
+	}
+
+	/**
 	 * Test case.
 	 */
 	function test_generates_permalink_to_post_while_missing_meta_key() {
@@ -26,14 +49,16 @@ class PostWithDynamicallyCreatedMetaKey extends BaseTestCase {
 		];
 		$created_post_id = $this->factory()->post->create( $post_params );
 
+		$this->given_hook_registered();
+
 		// when & then.
-		$this->permalink_asserter->has_permalink( $created_post_id, '/some-post-title/' );
+		$this->permalink_asserter->has_permalink( $created_post_id, '/default-value/some-post-title/' );
 	}
 
 	/**
 	 * Test case.
 	 */
-	function test_404_when_missing_meta_key() {
+	function test_go_to_post_when_dynamic_generated_meta_value_without_hook() {
 		// given.
 		$this->permalink_steps->given_permalink_structure( '/%field_some_meta_key%/%postname%/' );
 
@@ -47,7 +72,7 @@ class PostWithDynamicallyCreatedMetaKey extends BaseTestCase {
 		$created_post_id = $this->factory()->post->create( $post_params );
 
 		// when.
-		$this->go_to( '/inexisting-meta-value/some-post-title/' );
+		$this->go_to( '/default-value/some-post-title/' );
 
 		// then.
 		$this->navigation_asserter->then_not_displayed_post( $created_post_id )
@@ -58,22 +83,25 @@ class PostWithDynamicallyCreatedMetaKey extends BaseTestCase {
 	/**
 	 * Test case.
 	 */
-	function test_not_go_to_the_post_when_missing_meta_key_part_in_url() {
+	function test_go_to_post_when_dynamic_generated_meta_value_with_hook() {
 		// given.
 		$this->permalink_steps->given_permalink_structure( '/%field_some_meta_key%/%postname%/' );
 
 		$post_params     = [
 			'post_title' => 'Some post title',
 			'meta_input' => [
-				'some_meta_key' => 'Some meta value',
+				// There is missing meta key here
+				// 'some_meta_key' => 'Some meta value', .
 			],
 		];
 		$created_post_id = $this->factory()->post->create( $post_params );
 
+		$this->given_hook_registered();
+
 		// when.
-		$this->go_to( '/some-post-title/' );
+		$this->go_to( '/default-value/some-post-title/' );
 
 		// then.
-		$this->navigation_asserter->then_not_displayed_post( $created_post_id );
+		$this->navigation_asserter->then_displayed_post( $created_post_id );
 	}
 }
