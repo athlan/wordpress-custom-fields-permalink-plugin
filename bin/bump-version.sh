2140bcc8872417a3f6747069ea5cde8a394180d3
Athlan <athlan@vgroup.pl>
2018-04-23 20:18:09 +0200

    Bumping versions script

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -3,7 +3,7 @@
 usage() {
     echo "usage: $0 <version-type> <version-to-set>"
 	echo ""
-	echo "version-type: plugin_version"
+	echo "version-type: plugin_version|wp_min|wp_to|php_min"
 	echo ""
 }
 
@@ -15,11 +15,55 @@ fi
 version_type="handle_$1"
 version_to_bump="$2"
 
+file_plugin_main="wordpress-custom-fields-permalink-plugin.php"
+file_readme_repo="README.md"
+file_readme_wp="readme.txt"
+file_travisci=".travis.yml"
+
 handle_plugin_version() {
-    echo "123";
+    echo "Starting bumping plugin version to $version_to_bump"
+
+    sed -i -E "s/Version: (.+)/Version: $version_to_bump/" "$file_plugin_main"
+    echo "Version bumped in $file_plugin_main"
+
+    sed -i -E "s/Stable tag: (.+)/Stable tag: $version_to_bump/" "$file_readme_repo"
+    echo "Version bumped in $file_readme_repo"
+
+    sed -i -E "s/Stable tag: (.+)/Stable tag: $version_to_bump/" "$file_readme_wp"
+    echo "Version bumped in $file_readme_wp"
 }
 
-if [ $(type -t "$version_type") == function ]; then
+handle_wp_min() {
+    echo "Starting bumping minimum Word Press version to $version_to_bump"
+
+    sed -i -E "s/Requires at least: (.+)/Requires at least: $version_to_bump/" "$file_readme_repo"
+    echo "Version bumped in $file_readme_repo"
+
+    sed -i -E "s/Requires at least: (.+)/Requires at least: $version_to_bump/" "$file_readme_wp"
+    echo "Version bumped in $file_readme_wp"
+}
+
+handle_wp_to() {
+    echo "Starting bumping tested up to Word Press version to $version_to_bump"
+
+    sed -i -E "s/Tested up to: (.+)/Tested up to: $version_to_bump/" "$file_readme_repo"
+    echo "Version bumped in $file_readme_repo"
+
+    sed -i -E "s/Tested up to: (.+)/Tested up to: $version_to_bump/" "$file_readme_wp"
+    echo "Version bumped in $file_readme_wp"
+}
+
+handle_php_min() {
+    echo "Starting bumping php min version to $version_to_bump"
+
+    sed -i -E "s/Requires PHP: (.+)/Requires PHP: $version_to_bump/" "$file_readme_repo"
+    echo "Version bumped in $file_readme_repo"
+
+    sed -i -E "s/Requires PHP: (.+)/Requires PHP: $version_to_bump/" "$file_readme_wp"
+    echo "Version bumped in $file_readme_wp"
+}
+
+if [ $(type -t $version_type) == function ]; then
     $version_type
 else
     usage
