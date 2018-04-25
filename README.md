e98dd747741c862baf4d120960ecf16498c306cc
Piotr Pelczar <athlan@vgroup.pl>
2018-04-25 19:24:39 +0200

    Code Coverage (#21)
    * Coverage added
    * phpunit whitelist
    * XDebug enabled
    * Add badge
    * Move entrypoint to main.php
    * codecov commit status

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Plugin allows to use post's custom fields values in permalink structure by adding %field_fieldname%, for posts, pages and custom post types.
 
 [![Build Status](https://travis-ci.org/athlan/wordpress-custom-fields-permalink-plugin.svg?branch=master)](https://travis-ci.org/athlan/wordpress-custom-fields-permalink-plugin)
+[![codecov](https://codecov.io/gh/athlan/wordpress-custom-fields-permalink-plugin/branch/master/graph/badge.svg)](https://codecov.io/gh/athlan/wordpress-custom-fields-permalink-plugin)
 
 ---
 
--- wordpress-custom-fields-permalink-plugin.php
+++ wordpress-custom-fields-permalink-plugin.php
@@ -13,12 +13,5 @@
  * Author URI: http://athlan.pl/
  */
 
-// Require main class.
-require 'includes/class-customfieldspermalink.php';
-
-add_filter( 'pre_post_link', array( 'CustomFieldsPermalink', 'link_post' ), 100, 3 );
-add_filter( 'post_type_link', array( 'CustomFieldsPermalink', 'link_post_type' ), 100, 4 );
-add_filter( 'rewrite_rules_array', array( 'CustomFieldsPermalink', 'rewrite_rules_array_filter' ) );
-add_filter( 'query_vars', array( 'CustomFieldsPermalink', 'register_extra_query_vars' ), 10, 1 );
-add_filter( 'request', array( 'CustomFieldsPermalink', 'process_request' ), 10, 1 );
-add_filter( 'pre_handle_404', array( 'CustomFieldsPermalink', 'pre_handle_404' ), 10, 2 );
+// Require main entry point.
+require 'includes/main.php';
